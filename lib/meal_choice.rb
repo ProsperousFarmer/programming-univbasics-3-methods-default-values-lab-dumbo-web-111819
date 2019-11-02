f15f9e1f102edac5dfcc17ebcf4c4428713e0b08
Ben Miriello <benmiriello@gmail.com>
2019-11-02 22:35:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@ def meal_choice (veg1, veg2, protein = "meat")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-puts meal_choice.inspect
+# puts meal_choice.inspect
 
 # meal_choice("broccoli", "macaroni")
 
